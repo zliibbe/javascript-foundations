d9260b5fdb57ff48996ae2e1c6a0c34a552bb4d2
Zach Liibbe <zliibbe@gmail.com>
2023-04-06 11:42:48 -0600

    Pass all Dragon tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,14 +1,38 @@
-const createDragon = (name) =>  {
-  var dragon = {
-      name: name
-    }
+const createDragon = (name, rider, temperment) =>  {
 
-    return dragon
+  return dragon = {
+      name: name,
+      rider: rider, 
+      temperment: temperment,
+      timesEaten: 0,
+      hungry: true
+    }
+}
+
+const greetRider = (dragonObj) => {
+
+  return `Hi, ${dragonObj.rider}!`
+}
+
+const eat = (dragonObj) => {
+  if (dragonObj.timesEaten < 3) {
+    dragonObj.timesEaten++
+  } 
+  
+  if (dragonObj.timesEaten > 2) {
+    dragonObj.hungry=false
+  }
+
+  return dragonObj
+}
+
+const findFireBreathers = (dragonArr) => {
+  return dragonArr.filter((dragon) => dragon.temperment === 'aggressive')
 }
 
 module.exports = {
   createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
