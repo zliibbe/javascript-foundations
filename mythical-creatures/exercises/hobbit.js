3bb2691a4a2353d21552b93884ed0cba4ca9ac65
Zach Liibbe <zliibbe@gmail.com>
2023-04-07 09:14:59 -0600

    Pass majority hobbit tests

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,9 +1,53 @@
+const createHobbit = (name = 'unknown', age = '0') => {
+  return {
+    name: name,
+    age: age,
+    adult: false,
+    isAdult: false,
+    isOld: false,
+    acquaintances: []
+  }  
+}
 
+const celebrateBirthday = (hobbit, yearsAged = 1) => {
+  if(hobbit.age > 31 && hobbit.isAdult === false) {
+    hobbit.isAdult = true
+  }
+  if(hobbit.age >= 100) {
+    hobbit.isOld = true
+  }
+  
+  hobbit.age++
+
+  return hobbit
+}
+
+const getRing = (hobbit) => {
+  if (hobbit.name === 'Frodo') {
+    return `Here is the ring!`
+  }
+  
+  return 'You can\'t have it!'
+}
+
+const meetPeople = (hobbitObj, peopleArr) => {
+  for (let i = 0; i<peopleArr.length; i++){
+    hobbitObj.acquaintances.push(peopleArr[i])
+  }
+  return hobbitObj
+}
+
+const findFriends = (hobbitObj) => {
+  console.log(hobbitObj);
+  return hobbitObj.acquaintances.filter( hobbit => {
+     return hobbit.relationship === 'friend'
+  })
+}
 
 module.exports = {
-  // createHobbit, 
-  // celebrateBirthday, 
-  // getRing, 
-  // meetPeople, 
-  // findFriends
+  createHobbit, 
+  celebrateBirthday, 
+  getRing, 
+  meetPeople, 
+  findFriends
 }
