c35e4ddd7b5d317c1288b7a6ac52d3e8c6eef4f3
Zach Liibbe <zliibbe@gmail.com>
2023-04-14 10:31:23 -0600

    Pass first vampire test

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,9 +1,30 @@
+const createVampire = (name) => {
+  return vampire = {
+    name: name,
+    pet: 'bat'
+  }
+}
 
+const drink = () => {
+  
+}
+
+const findBatLovers = () => {
+  
+}
+
+const encounterDeliciousVictim = () => {
+  
+}
+
+const inquirePlace = () => {
+  
+}
 
 module.exports = {
-  // createVampire, 
-  // drink, 
-  // findBatLovers, 
-  // encounterDeliciousVictim, 
-  // inquirePlace
+  createVampire, 
+  drink, 
+  findBatLovers, 
+  encounterDeliciousVictim, 
+  inquirePlace
 }
