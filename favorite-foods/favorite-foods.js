391aea7322cfaee68d74c05170ad611472eee29b
Zach Liibbe <zliibbe@gmail.com>
2023-04-18 08:41:14 -0600

    Pass favorite-food test

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -2,11 +2,26 @@ const createFavoriteFood = (dishObj) => {
     return {
       name: dishObj.dish,
       ingredients: dishObj.ingredients,
-      isSpicy: dishObj.isSpicy
+      isSpicy: dishObj.isSpicy,
+      timesOrdered: 0
     }
+}
+
+const commentOnSpiciness = (dishObj) => {
+  if(dishObj.isSpicy) {
+    return `Wow, this ${dishObj.name} is so spicy!`
+  }
+  
+  return `Phew, this ${dishObj.name} is not very spicy.`
+}
+
+const orderFood = () => {
+
 }
 
 
 module.exports = { 
-  createFavoriteFood
+  createFavoriteFood,
+  commentOnSpiciness,
+  orderFood
 };
